5d7773822b8484bfbd443e53b5ef59cf5294bfab
unknown <joel5174@OLY-MCCUNE7L.esri.com>
2015-10-07 16:27:11 -0700

    remove unneeded chapter 05 directory

--- ch05-mongoose/app/routes/users.server.routes.js
+++
@@ -1,9 +0,0 @@
-// Load the users controller
-var users = require('../../app/controllers/users.server.controller');
-
-// Define the routes module method
-module.exports = function(app) {
-  // Set up the 'users' base routes
-  app.route('/users')
-    .post(users.create);
-};
--- ch05-mongoose/config/config.js
+++
@@ -1,4 +0,0 @@
-/* globals require, module */
-
-// based on the environment, load the correct settings file
-module.exports = require('./env/' + process.env.NODE_ENV);
